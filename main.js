719af7a55aeffa50abcd4f6863459a9da74fa506
ecy20 <e.adomako@amalitech.org>
2023-07-01 14:40:22 +0000

    completion phase 1

--- main.js
+++ main.js
@@ -1,15 +1,69 @@
 import database from './fake-api.json' assert { type: "json" };
 
+function modifyListProperties (todoList) {
+  todoList.classList.add("task-list");
+  let items = todoList.getElementsByTagName("li"), current = null;
+console.log(items)
+  for (let i of items) {
+    i.draggable = true;
+    i.ondragstart = e => {
+      current = i;
+      for (let it of items) {
+        if (it != current) { it.classList.add("hint"); }
+      }
+    };
+    i.ondragenter = e => {
+      if (i != current) { i.classList.add("active"); }
+    };
+    i.ondragleave = () => i.classList.remove("active");
+
+    i.ondragend = () => { for (let it of items) {
+        it.classList.remove("hint");
+        it.classList.remove("active");
+    }};
+    i.ondragover = e => e.preventDefault();
+    i.ondrop = e => {
+      // e.preventDefault();
+      if (i != current) {
+        let currentpos = 0, droppedpos = 0;
+        for (let it=0; it<items.length; it++) {
+          if (current == items[it]) { currentpos = it; }
+          if (i == items[it]) { droppedpos = it; }
+        }
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling);
+        } else {
+          i.parentNode.insertBefore(current, i);
+        }
+      }
+    };
+  }
+}
+
+
+function refresh() {
+    // document.getElementsByTagName("ul")[0].innerHTML = ''
+    for (var i = 0; i < database.tasks.length; i++) {
+        document.getElementsByTagName("ul")[0].innerHTML+= 
+        "<li>"+
+            "<span>"+database.tasks[i].todo+"</span>"+
+            "<span class='delete-task' id='remove'>"+"x"+"</span>"+
+        "</li>"
+      };
+}
 function addTask(taskname) {
     database.tasks.push({"id": idGen(), "todo":taskname, "active":true, "completed":false})
+    document.getElementsByTagName("ul")[0].innerHTML = ''
+    refresh()
+    modifyListProperties(document.getElementById("sortlist"))
     console.log(database)
 }
 
 function deleteTask(id) {
-    const arr2 = database.tasks.filter(x => x.id !== id);
-    // database.tasks=arr2
-    console.log(database.tasks);
-    console.log(arr2); 
+    console.log(id)
+    // const arr2 = database.tasks.filter(x => x.id !== id);
+    // console.log(database.tasks);
+    // console.log(arr2); 
 }
 
 function idGen(){
@@ -21,10 +75,16 @@ function idGen(){
   
    return id;
   }
-  addTask("new task3");
 
-  for (var i = 0; i < database.tasks.length; i++) {
-    document.getElementsByTagName("table")[0].innerHTML+= "<div class='todo-item'>"+database.tasks[i].todo+"</div>"
-  };
-// addTask("new task3");
-// deleteTask("4f5eef");
+
+document.getElementById("addTodo").addEventListener(
+  "keyup",
+  (e) => {
+    if (e.key === "Enter") {
+      // console.log(document.getElementById("addTodo").value)
+      addTask(document.getElementById("addTodo").value);
+      document.getElementById("addTodo").value = ''
+    }
+});
+refresh()
+modifyListProperties(document.getElementById("sortlist"))
